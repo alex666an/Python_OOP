eaa041a02d47126ed7284e573f100abaac045fbb
alex666an <141152186+alex666an@users.noreply.github.com>
2024-12-11 12:27:47 +0200

    Add files via upload

---
+++ Guild_system/project/player.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+class Player:
+    DEFAULT_GUILD = "Unaffiliated"
+
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = self.DEFAULT_GUILD
+
+    def add_skill(self, skill_name, mana_cost):
+        if self.skills.get(skill_name):
+            return "Skill already added"
+
+        self.skills[skill_name] = mana_cost
+
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        skills_string = "\n".join(f"==={name} - {mp}" for name, mp in self.skills.items())
+        return f"Name: {self.name}\n" \
+               f"Guild: {self.guild}\n" \
+               f"HP: {self.hp}\n" \
+               f"MP: {self.mp}\n" \
+               f"{skills_string}"
+
+
